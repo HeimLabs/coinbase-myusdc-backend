522e2d27c76dd048821ef5138dff0e39f07413af
Van Hohenheim <0xhohenheim@gmail.com>
2024-09-08 23:06:52 +0530

    rm: single wallet code

--- controllers/wallet.controllers.ts
+++ controllers/wallet.controllers.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import AppError from "../utils/appError";
-import { wallet as CoinbaseWallet } from "../services/coinbase.services";
+// import { wallet as CoinbaseWallet } from "../services/coinbase.services";
 import { AssetTransferRequest } from "types/api.types";
 import { Coinbase } from "@coinbase/coinbase-sdk";
 
 export async function getWallet(req: Request, res: Response, next: NextFunction) {
     try {
-        const addressData = CoinbaseWallet.getDefaultAddress();
+        // const addressData = CoinbaseWallet.getDefaultAddress();
 
-        return res.status(200).json({
-            address: addressData?.getId(),
-            chain: addressData?.getNetworkId()
-        });
+        // return res.status(200).json({
+        //     address: addressData?.getId(),
+        //     chain: addressData?.getNetworkId()
+        // });
     } catch (error) {
         next(error);
     }
@@ -27,24 +27,24 @@ export async function transferAsset(req: AssetTransferRequest, res: Response, ne
             throw new AppError(400, "error", "Invalid request");
 
         if (token == Coinbase.assets.Usdc) {
-            const balance = await CoinbaseWallet.getBalance(token);
-            if (balance.lessThan(amount))
-                throw new AppError(400, "error", "Insufficient balance");
+            // const balance = await CoinbaseWallet.getBalance(token);
+            // if (balance.lessThan(amount))
+            //     throw new AppError(400, "error", "Insufficient balance");
         }
         else {
             throw new AppError(400, "error", "Unsupported token");
         }
 
-        const transfer = await CoinbaseWallet.createTransfer({
-            amount: parseFloat(amount),
-            assetId: token,
-            destination: wallet,
-            gasless: token == Coinbase.assets.Usdc ? true : false
-        });
+        // const transfer = await CoinbaseWallet.createTransfer({
+        //     amount: parseFloat(amount),
+        //     assetId: token,
+        //     destination: wallet,
+        //     gasless: token == Coinbase.assets.Usdc ? true : false
+        // });
 
         return res.status(200).json({
-            transactionLink: transfer.getTransactionLink(),
-            status: transfer.getStatus()
+            // transactionLink: transfer.getTransactionLink(),
+            // status: transfer.getStatus()
         });
     } catch (error) {
         next(error);
